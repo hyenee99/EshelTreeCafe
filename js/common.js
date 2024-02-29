790b0e5358e070cda6bf93173bc1ce1ad46c6637
hyenee99 <twoddal2@naver.com>
2024-02-29 23:44:39 +0900

    인기메뉴 swiper 생성

--- js/common.js
+++ js/common.js
@@ -16,3 +16,21 @@ function showSlides() {
 
   setTimeout(showSlides, 3000); // 3초마다 이미지가 체인지됩니다.
 }
+
+new Swiper('.top_menu .swiper-container',{
+  slidesPerView: 3, //한 번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백
+  centeredSlides: true, //1번 슬라이드가 가운데 보이기
+  loop: true,
+  autoplay: {
+    delay: 2000
+  },
+  pagination:{
+    el:'.top_menu .swiper-pagination', //페이지 번호 요소 선택자 
+    clickable:true 
+  },
+  navigation:{
+    prevEl:'.top_menu .swiper-button-prev',
+    nextEl:'.top_menu .swiper-button-next'
+  }
+});
