fd541d5996f21f6cc67dd4b88595ea4dc472bd0c
hyenee99 <twoddal2@naver.com>
2024-03-01 22:12:20 +0900

    컨디션 버튼에 따른 추천 메뉴 이미지 변경

--- js/common.js
+++ js/common.js
@@ -34,3 +34,36 @@ new Swiper('.top_menu .swiper-container',{
     nextEl:'.top_menu .swiper-button-next'
   }
 });
+
+const fatigue=document.querySelector("#fatigue");
+const cold=document.querySelector("#cold");
+const indigestion=document.querySelector("#indigestion");
+const beauty=document.querySelector("#beauty");
+
+const img_1=document.querySelector("#change_1");
+const img_2=document.querySelector("#change_2");
+const img_3=document.querySelector("#change_3");
+
+fatigue.addEventListener("click",()=> { //피로해소 버튼 눌렀을 때
+  img_1.src="./images/jamong.png";
+  img_2.src="./images/maesil.png";
+  img_3.src="./images/jujube.png";
+});
+
+cold.addEventListener("click",()=> { //감기 버튼 눌렀을 때
+  img_1.src="./images/ginger.png";
+  img_2.src="./images/jujube.png";
+  img_3.src="./images/yuja.png";
+});
+
+indigestion.addEventListener("click",()=> { //소화불량 버튼 눌렀을 때 
+  img_1.src="./images/maesil.png";
+  img_2.src="./images/appleCin.png";
+  img_3.src="./images/lemon.png";
+});
+
+beauty.addEventListener("click",()=> { //미용 버튼 눌렀을 때 
+  img_1.src="./images/lemon.png";
+  img_2.src="./images/jamong.png";
+  img_3.src="./images/tangerine.png";
+});
